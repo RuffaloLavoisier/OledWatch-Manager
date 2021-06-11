c024d7ca87ab4195b8f48f798b7723282c4aa14a
RuffaloLavoisier <jsh970307@daum.net>
2021-06-11 17:53:55 +0900

    add Bluetooth push app

---
+++ oledwatch-APP/app/src/main/java/com/polkapolka/bluetooth/le/MyNotificationListener.java
@@ -0,0 +1,48 @@
+package com.polkapolka.bluetooth.le;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.graphics.drawable.Icon;
+import android.os.Build;
+import android.os.Bundle;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyNotificationListener extends NotificationListenerService {
+    public final static String TAG = "MyNotificationListener";
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        super.onNotificationRemoved(sbn);
+
+        Log.d(TAG, "onNotificationRemoved ~ " +
+                " packageName: " + sbn.getPackageName() +
+                " id: " + sbn.getId());
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        super.onNotificationPosted(sbn);
+
+        Notification notification = sbn.getNotification();
+        Bundle extras = sbn.getNotification().extras;
+        String title = extras.getString(Notification.EXTRA_TITLE);
+        CharSequence text = extras.getCharSequence(Notification.EXTRA_TEXT);
+        CharSequence subText = extras.getCharSequence(Notification.EXTRA_SUB_TEXT);
+    //    Icon smallIcon = notification.getSmallIcon();
+     //   Icon largeIcon = notification.getLargeIcon();
+
+        Log.d("CatchBug" , "onNotificationPosted ~ " +
+                " packageName: " + sbn.getPackageName() +
+                " id: " + sbn.getId() +
+                " postTime: " + sbn.getPostTime() +
+                " title: " + title +
+                " text : " + text +
+                " subText: " + subText);
+
+    }
+
+}
