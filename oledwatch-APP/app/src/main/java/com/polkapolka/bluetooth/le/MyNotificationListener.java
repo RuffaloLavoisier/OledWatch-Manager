2912cd869ed66eae46257ec30dff69847d136927
RuffaloLavoisier <jsh970307@daum.net>
2021-07-28 16:17:48 +0900

    Add : Save Msg .txt

--- oledwatch-APP/app/src/main/java/com/polkapolka/bluetooth/le/MyNotificationListener.java
+++ oledwatch-APP/app/src/main/java/com/polkapolka/bluetooth/le/MyNotificationListener.java
@@ -2,18 +2,33 @@ package com.polkapolka.bluetooth.le;
 
 import android.annotation.TargetApi;
 import android.app.Notification;
+import android.content.Context;
 import android.graphics.drawable.Icon;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 public class MyNotificationListener extends NotificationListenerService {
     public final static String TAG = "MyNotificationListener";
     public static String title;
     public static CharSequence text;
+
+    final static String folder_name = Environment.getRootDirectory().getAbsolutePath()+"/"+"TestLog";
+    final static String filename = "logfile.txt";
+
 
     // 1 : kakao, 2 : call, 3 : msg
     public static int send_msg;
@@ -44,8 +59,8 @@ public class MyNotificationListener extends NotificationListenerService {
         // Output Toast message
         switch (sbn.getPackageName()){
             case "com.kakao.talk":
-                if(sbn.getId()==2 && subText==null) {   //
-                    Toast.makeText(this, "kakao msg : " + title + " / " + text, Toast.LENGTH_SHORT).show();
+                if(sbn.getId()==2 ) {   //
+                    Toast.makeText(this, "kakao msg : " +subText+ " : "+ title + " -> " + text, Toast.LENGTH_LONG).show();
                     send_msg=1;
                     DeviceControlActivity.makeChange();
                 }
@@ -62,15 +77,30 @@ public class MyNotificationListener extends NotificationListenerService {
                 DeviceControlActivity.makeChange();
                 break;
         }
-
         // Log Output
+
+
         Log.d("MSGBug" , "onNotificationPosted ~ " +
-                " packageName: " + sbn.getPackageName() +
+                " packageName: " + sbn.getPackageName() + //
                 " id: " + sbn.getId() +
                 " postTime: " + sbn.getPostTime() +
-                " title: " + title +
-                " text : " + text +
-                " subText: " + subText);
-    }
+                " title: " + title + //
+                " text : " + text + //
+                " subText: " + subText ); //
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String msg_content=now+" "+sbn.getPackageName()+" "+subText+" // "+title+" // "+text+"\n\n";
 
+      //  WriteTextFile(folder_name, filename,msg_content);
+
+
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) ,"/_MyLog_File" + ".txt");
+        //Toast.makeText(this, file+"", Toast.LENGTH_SHORT).show();
+        try {
+            FileWriter out = new FileWriter(file,true);
+            out.write(msg_content);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
